3c71b43d751922f7bc0f0302b402f1ec28ae3a47
alexx <alexx-gsm@yandex.ru>
2016-03-29 01:50:03 +0700

    db execute

--- App/Db.php
+++ App/Db.php
@@ -12,9 +12,20 @@ class Db
         $this->dbh = new \PDO('mysql:host=127.0.0.1; dbname=php', 'alexx', '15987');
     }
 
-    public function execute( $sql )
+    public function execute($sql)
     {
         $sth = $this->dbh->prepare($sql);
-        return $sth->execute();
+        $res = $sth->execute();
+        return $res;
+    }
+
+    public function query($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        if( false !== $res ) {
+            return $sth->fetchAll();
+        }
+        return [];
     }
 }
